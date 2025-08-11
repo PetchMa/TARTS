3b058d56b7fcbc0a3745b77619344870853d4909
PetchMa <peterxy.ma@gmail.com>
2025-08-11 18:00:51 -0400

    add conda_build_config.yaml

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tarts",
-    version="0.0.0a6",
+    version="0.0.0a7",
     packages=find_packages(),
     install_requires=[],
     author="Peter Xiangyuan Ma",
